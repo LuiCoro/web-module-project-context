84d88b0502a67d51e5d405e35a92d0641a814d61
LuiCoro <luiacorona029@icloud.com>
2021-09-23 16:44:05 -0700

    ' Task 1 Completed ' - addItem now works correctly
    ' Task 2 Completed '
    - created a folder called 'context' that holds our createContext() called
     (ProductContext)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Route} from 'react-router-dom';
 import data from './data';
 
 // Components
@@ -8,27 +8,28 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
-
-	const addItem = item => {
-		// add the given item to the cart
-	};
-
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
+  
+  const addItem = item => {
+    // add the given item to the cart
+    setCart([...cart, item]);
+  };
+  
+  return (
+    <div className="App">
+      <Navigation cart={cart}/>
+      
+      {/* Routes */}
+      <Route exact path="/">
+        <Products products={products} addItem={addItem}/>
+      </Route>
+      
+      <Route path="/cart">
+        <ShoppingCart cart={cart}/>
+      </Route>
+    </div>
+  );
 }
 
 export default App;
