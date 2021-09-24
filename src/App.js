27b802d650c0f076ec48b6f15564bc5cfc612eb9
LuiCoro <luiacorona029@icloud.com>
2021-09-23 20:46:25 -0700

    'All Tasks Completed' - A new file was created for our cart context - got rid of all props inside of Navigation.js and ShoppingCart.js - Had to fix how I wrapped my components was causing errors like crazy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import {Route} from 'react-router-dom';
 import data from './data';
+import {ProductContext} from "./contexts/ProductContext";
+import {CartContext} from "./contexts/CartContext";
 
 // Components
 import Navigation from './components/Navigation';
@@ -18,16 +20,19 @@ function App() {
   
   return (
     <div className="App">
+      <ProductContext.Provider value={{products, addItem}}>
+        <CartContext.Provider value={cart}>
       <Navigation cart={cart}/>
-      
-      {/* Routes */}
-      <Route exact path="/">
-        <Products products={products} addItem={addItem}/>
-      </Route>
-      
-      <Route path="/cart">
-        <ShoppingCart cart={cart}/>
-      </Route>
+          {/* Routes */}
+          <Route exact path="/">
+            <Products/>
+          </Route>
+          
+          <Route path="/cart">
+            <ShoppingCart cart={cart}/>
+          </Route>
+        </CartContext.Provider>
+      </ProductContext.Provider>
     </div>
   );
 }
